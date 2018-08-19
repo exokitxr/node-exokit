37a34f77eb25bb3a466acafe70ff092c9a5ca6c1
Avaer Kazmer <a@modules.io>
2018-08-19 09:45:08 -0400

    Small build-ml script cleanup

--- build-ml.sh
+++ build-ml.sh
@@ -14,8 +14,7 @@ make -j4
 cp -f torque out/Release/
 find . -type f -name '*.d' -exec sed -i 's/C:\\/\/mnt\/c\//g' "{}" +;
 find . -type f -name '*.d' -exec sed -i 's/C:/\/mnt\/c\//g' "{}" +;
-find . -type f -name '*.d' -exec sed -i 's/\([^ ]\)\\/\1\//g' "{}" +;
-git checkout src/*.d
+find . -type f -name '*.d' -exec sed -i 's/\([^ 	]\)\\/\1\//g' "{}" +;
 make -j4
 
 rm -Rf libnode.a
