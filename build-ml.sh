b58600a36b2ee0bf76c6a48ddc725a638939663a
Avaer Kazmer <a@modules.io>
2018-08-19 08:50:16 -0400

    Major build-ml script update

--- build-ml.sh
+++ build-ml.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+git checkout base
+./configure
+make -j4
+cp out/Release/torque .
+
+git checkout master
+rm -Rf out
+../magicleap-js/hack-toolchain.js
 ./android-configure
+make -j4
 
-cp /mnt/c/Users/avaer/Documents/GitHub/node2/out/Release/torque /mnt/c/Users/avaer/Documents/GitHub/node/out/Release/torque
-
+cp -f torque out/Release/
 find . -type f -name '*.d' -exec sed -i 's/C:\\/\/mnt\/c\//g' "{}" +;
 find . -type f -name '*.d' -exec sed -i 's/C:/\/mnt\/c\//g' "{}" +;
 find . -type f -name '*.d' -exec sed -i 's/\([^ ]\)\\/\1\//g' "{}" +;
-
 make -j4
 
 rm -Rf libnode.a
