f8a10be563a576ea8e629bda53f803640708f397
avaer <a@modules.io>
2018-11-16 16:54:31 -0500

    Move git checkout in build-ml.sh script

--- build-ml.sh
+++ build-ml.sh
@@ -5,12 +5,13 @@ git checkout base
 make -j4
 cp out/Release/torque .
 
+git checkout 11.2.0
+
 rm -Rf out
 ../magicleap-js/hack-toolchain.js
 ./android-configure
 make -j4
 
-git checkout 11.2.0
 cp -f torque out/Release/
 find . -type f -name '*.d' -exec sed -i 's/C:\\/\/mnt\/c\//g' "{}" +;
 find . -type f -name '*.d' -exec sed -i 's/C:/\/mnt\/c\//g' "{}" +;
